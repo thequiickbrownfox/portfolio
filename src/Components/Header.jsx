76b4e77ebf2a380224322414047757a2c1de34b3
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2023-08-29 15:02:00 +0000

    added 'CV/Resume' header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -24,6 +24,9 @@ const Header = () => {
       <a href="#home">Home</a>
       <a href="#about">About</a>
       <a href="#portfolio">Portfolio</a>
+      <a href="https://read.cv/abinvarghese" target="_blank" rel="noopener noreferrer">
+        CV/Resume
+      </a>
       <a href="https://medium.com/@abin_varghese" target="_blank" rel="noopener noreferrer">
         Blogs
       </a>
