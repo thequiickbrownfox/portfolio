aa97baf0161a17ddfc91577bc7d0bba823b592aa
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2023-10-04 10:49:43 +0000

    goof

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/2 hd.png";
+import image from "../images/rania.jpeg";
 
 // const imageAltText = "";
 
